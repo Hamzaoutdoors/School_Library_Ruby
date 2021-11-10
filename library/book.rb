030bbe9e64d530ae6a1650abd5209e03681d0e7f
Hamzaoutdoors <ellaouzihamza@gmail.com>
2021-11-11 00:38:32 +0100

    feat : Added Book class

---
+++ library/book.rb
@@ -0,0 +1,18 @@
+require './library/rental'
+
+# Book class
+
+class Book
+  attr_reader :rentals
+  attr_accessor :title, :author
+
+  def initialize(title, author)
+    @title = title
+    @author = author
+    @rentals = []
+  end
+
+  def add_rental(date, person)
+    Rental.new(date, person, self)
+  end
+end
